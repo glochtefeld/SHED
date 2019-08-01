5b0b3cb15b4596527816bf5cbd5f4d275f4fa92a
Gavin Lochtefeld <lochga01@luther.edu>
2019-07-31 19:56:59 -0500

    Dialogue prereqs added

---
+++ README.md
@@ -0,0 +1,19 @@
+# Stardew Heart Event Designer
+
+This is the source code repository of SHED. It's still in a *very* alpha stage as of 2019-07-30, but that's hopefully
+going to change!
+
+If you have any questions about the repository, feel free to let me know. This project will be open source under the 
+MIT license. 
+
+## About
+SHED is a utility for modders of the game [Stardew Valley](stardewvalley.com). It will allow users to create and edit
+heart event data through a graphical user interface, so long as that data is in the JSON format. If you're looking to 
+start using SHED and don't know how to access event data, take a look through the 
+[Stardew Valley Wiki Page](stardewvalleywiki.com/Modding:Event_data) on the subject. When the project is complete, 
+a series of video tutorials will be linked to this page as well.
+
+## Credits
+This project was originally designed, envisioned, and created by Gavin Lochtefeld (@lochga01). 
+It is built with Java 12 and JavaFX 12, with heavy use of Scenebuilder. At some point, I'll take a UX design course,
+ but today is not that day.
--- src/main/java/com/gavinlochtefeld/shed/PreReqController.java
+++ src/main/java/com/gavinlochtefeld/shed/PreReqController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -18,6 +19,48 @@ public class PreReqController implements Initializable {
 
     // Context menu attributes
     private ObservableList<String> weathers = FXCollections.observableArrayList("rainy", "sunny");
+    private ObservableList<String> npcNames = FXCollections.observableArrayList(
+            "Alex",
+            "Elliot",
+            "Harvey",
+            "Sam",
+            "Shane",
+            "Abigail",
+            "Emily",
+            "Haley",
+            "Leah",
+            "Maru",
+            "Penny",
+            "Caroline",
+            "Clint",
+            "Demetrius",
+            "Dwarf",
+            "Evelyn",
+            "George",
+            "Gus",
+            "Jas",
+            "Jodi",
+            "Kent",
+            "Krobus",
+            "Lewis",
+            "Linus",
+            "Marnie",
+            "Pam",
+            "Pierre",
+            "Robin",
+            "Sandy",
+            "Vincent",
+            "Willy",
+            "Wizard",
+            "Gil",
+            "Governer",
+            "Grandpa",
+            "Gunther",
+            "Henchman",
+            "Marlon",
+            "Morris",
+            "Mr. Qi"
+    );
 
     // Context Menu FXML Attributes
     @FXML
@@ -37,14 +80,52 @@ public class PreReqController implements Initializable {
     @FXML
     private CheckBox daySun;
     @FXML
+    private CheckBox festivalCheck;
+    @FXML
+    private TextField dialogueReqInput;
+    @FXML
+    private Label dialogueReqOutLabel = new Label("");
+    @FXML
+    private TextField yearEntry;
+    @FXML
+    private CheckBox seasonSpring;
+    @FXML
+    private CheckBox seasonSummer;
+    @FXML
+    private CheckBox seasonFall;
+    @FXML
+    private CheckBox seasonWinter;
+    @FXML
+    private Label npcVisibleLabel;
+    @FXML
+    private ComboBox<String> npcPickDropdown;
+    @FXML
+    private Button addDialogueButton;
+    @FXML
+    private Button addNPCVisButton;
+    @FXML
     private TextField idBox;
     @FXML
     private Label badIDLabel;
-
+    @FXML
+    private Button finishButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         weatherDropdown.setItems(weathers);
+        npcPickDropdown.setItems(npcNames);
+    }
+
+    @FXML
+    private void addDialoguePrereq(ActionEvent event) {
+        String dialogIn = dialogueReqInput.getText();
+        if (dialogueReqOutLabel.getText() == "") {
+            dialogueReqOutLabel.setText(dialogIn);
+        }
+        else {
+            dialogueReqOutLabel.setText(dialogueReqOutLabel.getText() + ", " + dialogIn);
+        }
     }
 
     @FXML
