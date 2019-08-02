520c21b20db6292d3211850d7b33569845ba1b82
Gavin Lochtefeld <lochga01@luther.edu>
2019-08-01 22:42:37 -0500

    More responsive design

--- src/main/java/com/gavinlochtefeld/shed/PreReqController.java
+++ src/main/java/com/gavinlochtefeld/shed/PreReqController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -36,11 +37,10 @@ public class PreReqController implements Initializable {
     @FXML
     private Label dialogueReqOutLabel = new Label("");
     @FXML
-    private Button addDialogueButton, addNPCVisButton, finishButton;
+    private Button addDialogueButton, addNPCVisButton, finishButton, checkIDButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
         weatherDropdown.setItems(StardewData.getWeathers());
         npcPickDropdown.setItems(StardewData.getNpcNames());
     }
@@ -57,16 +57,30 @@ public class PreReqController implements Initializable {
     }
 
     @FXML
-    private void buildPrerequisite(ActionEvent event) {
-        if (!StardewData.isFreeID(Integer.valueOf(idBox.getText()))) {
-            badIDLabel.setText("ID is already in base game");
+    private void addNPCVisPrereq(ActionEvent event) {
+        String npcIn = npcPickDropdown.getValue() + "\n";
+        npcVisibleLabel.setText(npcVisibleLabel.getText() + npcIn);
+    }
+
+    @FXML
+    private void checkID(ActionEvent event) {
+        if (idBox.getText().matches("\\d*")) {
+            if (!StardewData.isFreeID(Integer.parseInt(idBox.getText()))) {
+                badIDLabel.setTextFill(Color.RED);
+                badIDLabel.setText("ID is already in base game");
+            }
+            else {
+                badIDLabel.setTextFill(Color.GREEN);
+                badIDLabel.setText("Good ID");
+            }
         }
         else {
-            badIDLabel.setText("Good ID, data copied to clipboard");
+            badIDLabel.setTextFill(Color.RED);
+            badIDLabel.setText("Not a number");
         }
     }
 
-    private void setPrerequisite(Prerequisite pre) {
+    public void setPrerequisite(Prerequisite pre) {
         this.prereq = pre;
     }
 }
